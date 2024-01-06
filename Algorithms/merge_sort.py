1c48644a9d8dfc553b0d9aa7275b383729fe5639
Batool suheil <batool.kaddah@gmail.com>
2024-01-06 13:07:48 -0500

    Complete merge sort

--- Algorithms/merge_sort.py
+++ Algorithms/merge_sort.py
@@ -4,7 +4,11 @@ Time & Date: 12:34 p.m. 2024-01-06
 PyCharm
 """
 
+
 def merge_sort(array):
+    if len(array) <= 1:
+        return
+
     middle_point = len(array) // 2
     left_part = array[:middle_point]
     right_part = array[middle_point:]
@@ -20,7 +24,24 @@ def merge_sort(array):
         if left_part[left_array_index] < right_part[right_array_index]:
             array[sorted_index] = left_part[left_array_index]
             left_array_index += 1
-
         else:
             array[sorted_index] = right_part[right_array_index]
             right_array_index += 1
+        sorted_index += 1
+
+    while left_array_index < len(left_part):
+        array[sorted_index] = left_part[left_array_index]
+        left_array_index += 1
+        sorted_index += 1
+
+    while right_array_index < len(right_part):
+        array[sorted_index] = right_part[right_array_index]
+        right_array_index += 1
+        sorted_index += 1
+
+
+if __name__ == '__main__':
+    numbers = [4, 10, 6, 14, 2, 1, 8, 5]
+    print('Unsorted array: ', numbers)
+    merge_sort(numbers)
+    print('Sorted array: ',numbers )
